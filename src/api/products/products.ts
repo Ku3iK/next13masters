d290e579db4d39fbdecce7c957521e1c7297621e
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2023-09-16 17:01:10 +0200

    feat: add fetchers to get products by number and all products number

--- src/api/products/products.ts
+++ src/api/products/products.ts
@@ -19,6 +19,31 @@ export const getProductById = async (id: ProductListItemType["id"]) => {
 	return productListItemTypeToProductListItemType(productResponse);
 };
 
+export const getProductsByNumber = async (number: number, page?: number) => {
+	let baseUrl = `https://naszsklep-api.vercel.app/api/products?take=${number}`;
+
+	if (page !== undefined) {
+		const offset = number * (page - 1);
+		baseUrl = `https://naszsklep-api.vercel.app/api/products?offset=${offset}&take=${number}`;
+	}
+
+	const res = await fetch(baseUrl);
+
+	const productsResponse = (await res.json()) as ProductResponseItemTypes[];
+
+	const products = productsResponse.map(productListItemTypeToProductListItemType);
+
+	return products;
+};
+
+export const getNumberOfAllProducts = async () => {
+	const res = await fetch("https://naszsklep-api.vercel.app/api/products?take=-1");
+
+	const productsResponse = (await res.json()) as ProductResponseItemTypes[];
+
+	return productsResponse.length;
+};
+
 const productListItemTypeToProductListItemType = (
 	product: ProductResponseItemTypes,
 ): ProductListItemType => {
@@ -31,5 +56,6 @@ const productListItemTypeToProductListItemType = (
 			src: product.image,
 			alt: product.title,
 		},
+		description: product.description,
 	};
 };
