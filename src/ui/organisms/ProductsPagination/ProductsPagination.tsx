4793e7af64a92c19f97e722dc270a4f2614c85bc
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2023-09-17 19:18:21 +0200

    feat: set assertion for pagination button url

--- src/ui/organisms/ProductsPagination/ProductsPagination.tsx
+++ src/ui/organisms/ProductsPagination/ProductsPagination.tsx
@@ -1,5 +1,6 @@
 import { getMaxPages } from "@/utils/getMaxPagesNumber";
 import { PaginationButton } from "@/ui/atoms/PaginationButton/PaginationButton";
+import { type HrefType } from "@/ui/atoms/ActiveLink/ActiveLinkTypes";
 
 export const ProductsPagination = ({
 	currentPage = 1,
@@ -14,17 +15,26 @@ export const ProductsPagination = ({
 
 	return (
 		<div className="mx-auto mt-8 flex items-center justify-center gap-4">
-			<PaginationButton url={"/products/1"} disabled={currentPage <= 1}>
+			<PaginationButton url={"/products/1" as HrefType<string>} disabled={currentPage <= 1}>
 				{"<<"}
 			</PaginationButton>
-			<PaginationButton url={`/products/${currentPage - 1}`} disabled={currentPage <= 1}>
+			<PaginationButton
+				url={`/products/${currentPage - 1}` as HrefType<string>}
+				disabled={currentPage <= 1}
+			>
 				{"<"}
 			</PaginationButton>
 			<p>{currentPage}</p>
-			<PaginationButton url={`/products/${currentPage + 1}`} disabled={currentPage >= lastPage}>
+			<PaginationButton
+				url={`/products/${currentPage + 1}` as HrefType<string>}
+				disabled={currentPage >= lastPage}
+			>
 				{">"}
 			</PaginationButton>
-			<PaginationButton url={`/products/${lastPage}`} disabled={currentPage >= lastPage}>
+			<PaginationButton
+				url={`/products/${lastPage}` as HrefType<string>}
+				disabled={currentPage >= lastPage}
+			>
 				{">>"}
 			</PaginationButton>
 		</div>
