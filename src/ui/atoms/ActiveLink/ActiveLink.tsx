24595d901354caecffcfa65ca54b1185f7034592
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2023-09-17 19:16:40 +0200

    feat: generete staticly first 10 pages for products pagination

--- src/ui/atoms/ActiveLink/ActiveLink.tsx
+++ src/ui/atoms/ActiveLink/ActiveLink.tsx
@@ -5,15 +5,28 @@ import clsx from "clsx";
 import { usePathname } from "next/navigation";
 import { type ActiveLinkType } from "./ActiveLinkTypes";
 
-export const ActiveLink = ({ href, children }: ActiveLinkType) => {
+export const ActiveLink = ({
+	href,
+	className,
+	activeClassName,
+	exact,
+	children,
+}: ActiveLinkType) => {
 	const pathname = usePathname();
-	const isActive = pathname === href;
+	const matchedPath = (typeof href === "string" ? href : href.pathname) ?? null;
+	const isActive =
+		(matchedPath &&
+			pathname &&
+			(exact ? pathname === matchedPath : pathname.startsWith(matchedPath))) ||
+		false;
 
 	return (
 		<Link
 			href={href}
 			className={clsx(
-				"text-blue-400 transition-colors duration-300 ease-in-out hover:text-blue-600",
+				`text-blue-400 transition-colors duration-300 ease-in-out hover:text-blue-600 ${
+					isActive && activeClassName
+				} ${className && className}`,
 				{
 					underline: isActive,
 				},
