6c32928324f0011abc670338b10158e974fdf488
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2023-09-17 19:20:10 +0200

    feat: add see more products button and display first 20 products

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,8 +1,21 @@
-import { getProductsList } from "@/api/products/products";
+import Link from "next/link";
+import { getProductsByNumber } from "@/api/products/products";
 import { ProductList } from "@/ui/organisms/ProductList/ProductList";
 
 export default async function ProductsPage() {
-	const products = await getProductsList();
+	const products = await getProductsByNumber(20);
 
-	return <ProductList products={products} />;
+	return (
+		<>
+			<ProductList products={products} />
+			<div className="flex items-center justify-center">
+				<Link
+					href="/products/1"
+					className="mt-10 rounded-md border border-slate-200 px-4 transition-colors duration-300 ease-in-out hover:bg-slate-400 hover:text-white"
+				>
+					Zobacz więcej
+				</Link>
+			</div>
+		</>
+	);
 }
