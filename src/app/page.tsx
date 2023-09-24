2f0f7a341c4156e448cb70700ab938f8f2c60b00
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2023-09-24 14:19:20 +0200

    feat: display for correctly test end products list for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function HomePage() {
-	return <h1>Home Page</h1>;
+import { productsGetList } from "@/api/products/products";
+import { ProductList } from "@/ui/organisms/ProductList/ProductList";
+
+export default async function HomePage() {
+	const products = await productsGetList();
+
+	return (
+		<>
+			<h1>Home Page</h1>
+			<ProductList products={products} />
+		</>
+	);
 }
