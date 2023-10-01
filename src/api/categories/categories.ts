14793ecefe72f0f85b284106700ed1b5c889acda
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2023-10-02 00:41:11 +0200

    refactor: import positions

--- src/api/categories/categories.ts
+++ src/api/categories/categories.ts
@@ -1,5 +1,5 @@
-import { CategoryGetTotalProductsNumberDocument } from "@/gql/graphql";
 import { executeGraphql } from "../grapghqlApiInstance";
+import { CategoryGetTotalProductsNumberDocument } from "@/gql/graphql";
 
 export const categoryGetTotalProductsNumber = async () => {
 	const response = await executeGraphql(CategoryGetTotalProductsNumberDocument, {});
