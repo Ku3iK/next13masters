1d9b50e639246f87937647426aad4219cd747a1b
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2023-10-03 21:26:36 +0200

    feat: create functions to trigger cart mutations

---
+++ src/api/cart/cart.ts
@@ -0,0 +1,60 @@
+import { cookies } from "next/headers";
+import { executeGraphql } from "../grapghqlApiInstance";
+import {
+	CartGetByIdDocument,
+	CartCreateDocument,
+	ProductGetByIdDocument,
+	CartAddItemDocument,
+} from "@/gql/graphql";
+
+async function createCart() {
+	return executeGraphql({ query: CartCreateDocument });
+}
+
+export async function getCartById(cartId: string) {
+	return executeGraphql({ query: CartGetByIdDocument, variables: { id: cartId } });
+}
+
+export async function getCartFromCookies() {
+	const cartId = cookies().get("cartId")?.value;
+
+	if (cartId) {
+		const cart = await getCartById(cartId);
+
+		if (cart.order) {
+			return cart.order;
+		}
+	}
+}
+
+export async function getOrCreateCart() {
+	const existingCart = await getCartFromCookies();
+
+	if (existingCart) {
+		return existingCart;
+	}
+
+	const cart = await createCart();
+
+	if (!cart.createOrder?.id) {
+		throw new Error("Cart not created");
+	}
+
+	return cart.createOrder;
+}
+
+export async function addProductToCart(cartId: string, productId: string) {
+	const product = await executeGraphql({
+		query: ProductGetByIdDocument,
+		variables: { id: productId },
+	});
+
+	if (!product.product) {
+		throw new Error("Product not found");
+	}
+
+	await executeGraphql({
+		query: CartAddItemDocument,
+		variables: { cartId, productId, total: product.product.price },
+	});
+}
