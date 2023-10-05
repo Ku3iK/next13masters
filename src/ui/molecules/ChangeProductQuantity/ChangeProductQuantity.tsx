9ecb1f3c417972b7dbcc24928c623fc54e70abdd
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2023-10-05 22:11:16 +0200

    feat: add change product quantity component

---
+++ src/ui/molecules/ChangeProductQuantity/ChangeProductQuantity.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { experimental_useOptimistic as useOptimistic } from "react";
+import { changeItemQuantity } from "@/api/cart/actions";
+
+export const ChangeProductQuantity = ({
+	itemId,
+	quantity,
+}: {
+	itemId: string;
+	quantity: number;
+}) => {
+	const [optimisticQuantity, setOptimisticQuantity] = useOptimistic(quantity);
+
+	return (
+		<form>
+			<button
+				data-testid="decrement"
+				formAction={async () => {
+					setOptimisticQuantity(optimisticQuantity - 1);
+					await changeItemQuantity(itemId, optimisticQuantity - 1);
+				}}
+				className="ml-2 h-8 w-8 border bg-slate-50"
+			>
+				-
+			</button>
+			<span data-testid="quantity">{optimisticQuantity}</span>
+			<button
+				data-testid="increment"
+				formAction={async () => {
+					setOptimisticQuantity(optimisticQuantity + 1);
+					await changeItemQuantity(itemId, optimisticQuantity + 1);
+				}}
+				className="ml-2 h-8 w-8 border bg-slate-50"
+			>
+				+
+			</button>
+		</form>
+	);
+};
