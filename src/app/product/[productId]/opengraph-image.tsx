8123730f1cea8288a4122c55804d4090aea5811c
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2023-10-08 22:27:52 +0200

    feat: create open graph image for single product

---
+++ src/app/product/[productId]/opengraph-image.tsx
@@ -0,0 +1,39 @@
+import { ImageResponse } from "next/server";
+import { notFound } from "next/navigation";
+import { productGetById } from "@/api/products/products";
+
+export const runtime = "edge";
+
+export const alt = "About Acme";
+export const size = {
+	width: 1200,
+	height: 630,
+};
+
+export const generateAlt = async () => {};
+
+export const contentType = "image/png";
+
+export default async function Image({ params }: { params: { productId: string } }) {
+	const product = await productGetById(params.productId);
+
+	if (!product) notFound();
+
+	return new ImageResponse(
+		(
+			<div
+				style={{
+					fontSize: 128,
+					background: "white",
+					width: "100%",
+					height: "100%",
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+				}}
+			>
+				{product.name}
+			</div>
+		),
+	);
+}
