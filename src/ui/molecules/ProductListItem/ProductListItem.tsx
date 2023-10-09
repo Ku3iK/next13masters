e52cf727c1f136cdf75957c1db8e8cce37ce48d7
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2023-10-09 22:42:46 +0200

    feat: add optional product average rating data to product list item

--- src/ui/molecules/ProductListItem/ProductListItem.tsx
+++ src/ui/molecules/ProductListItem/ProductListItem.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 import { ProductCoverImage } from "@/ui/atoms/ProductListItemImage/ProductListItemImage";
 import { ProductListItemDescription } from "@/ui/atoms/ProductListItemDescription/ProductListItemDescription";
-import { type ProductListItemFragment } from "@/gql/graphql";
+import { type ProductWithAverageRating } from "@/utils/addAverageRatingToProducts";
 
-export const ProductListItem = ({ product }: { product: ProductListItemFragment }) => {
+export const ProductListItem = ({ product }: { product: ProductWithAverageRating }) => {
 	return (
 		<li>
 			<Link href={`/product/${product.id}`}>
@@ -12,6 +12,14 @@ export const ProductListItem = ({ product }: { product: ProductListItemFragment 
 						<ProductCoverImage image={{ src: product.images[0].url, alt: product.name }} />
 					)}
 					<ProductListItemDescription product={product} />
+					{product.averageRating && (
+						<p>
+							Ocena:
+							<span data-testid="product-rating" className="font-medium">
+								{product.averageRating.toFixed(2)}
+							</span>
+						</p>
+					)}
 				</article>
 			</Link>
 		</li>
