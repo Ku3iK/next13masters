ba070a2fb73aad30ada80beefd6482ee491d335f
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2024-06-03 23:49:48 +0200

    chore(test): github rules protection of branch v3

--- src/components/icons/MenuIcon.tsx
+++ src/components/icons/MenuIcon.tsx
@@ -7,7 +7,7 @@ export const MenuIcon = () => {
 			viewBox="0 0 24 24"
 			fill="none"
 			stroke="currentColor"
-			strokeWidth="2"
+			strokeWidth="5"
 			strokeLinecap="round"
 			strokeLinejoin="round"
 		>
