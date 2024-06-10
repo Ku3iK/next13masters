746bcd33afb463ecd7f748ba404b153af57044e8
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2024-06-10 23:28:50 +0200

    feat: use providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Navbar } from "@/ui/organisms/Navbar/Navbar";
+import { Providers } from "@/providers/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,13 +15,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 	return (
 		<html lang="pl">
 			<body className={inter.className}>
-				<Navbar />
-				<section className="mx-auto max-w-md p-12 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
-					{children}
-				</section>
-				<footer>
-					<p className="text-center text-sm text-gray-500">© 2023</p>
-				</footer>
+				<Providers>
+					<Navbar />
+					<section className="mx-auto max-w-md p-12 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
+						{children}
+					</section>
+					<footer>
+						<p className="text-center text-sm text-gray-500">© 2023</p>
+					</footer>
+				</Providers>
 			</body>
 		</html>
 	);
