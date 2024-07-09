1b72ca173398068f72bf96b00b332fc1d55ea817
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2024-07-09 16:38:20 +0200

    feat: use schema to validate params on api route to add new review for product

--- src/app/api/addReview/route.ts
+++ src/app/api/addReview/route.ts
@@ -1,19 +1,29 @@
 import { type NextRequest, NextResponse } from "next/server";
+import { type ZodIssue } from "zod";
 import { addReviewToProduct } from "@/services/api/reviews/addReviewToProduct";
 import { type AddReviewToProductParams } from "@/services/api/reviews/addReviewToProduct/types";
 import { type ApiResponse } from "@/services/types";
+import { addReviewFormSchema } from "@/lib/validators/ProductReviewFormValidator/ProductReviewFormValidator";
 
 export async function POST(req: NextRequest) {
 	"use server";
 
-	// TODO: add validation by using a zod schema
-
-	const params = (await req.json()) as AddReviewToProductParams;
-
 	try {
+		const params = (await req.json()) as AddReviewToProductParams;
+
+		const validation = addReviewFormSchema.safeParse(params);
+
+		if (!validation.success) {
+			const response: ApiResponse<null, ZodIssue[]> = {
+				data: null,
+				error: validation.error.errors,
+			};
+			return NextResponse.json(response, { status: 400 });
+		}
+
 		const reviewId = await addReviewToProduct(params);
 
-		const response: ApiResponse<{ reviewId: string }> = {
+		const response: ApiResponse<{ reviewId: string }, null> = {
 			data: { reviewId: reviewId ?? "" },
 			error: null,
 		};
@@ -22,7 +32,7 @@ export async function POST(req: NextRequest) {
 	} catch (error) {
 		console.error("Error in addReviewToProduct:", error);
 
-		const response: ApiResponse<null> = {
+		const response: ApiResponse<null, string> = {
 			data: null,
 			error: "An error occurred while adding the review",
 		};
